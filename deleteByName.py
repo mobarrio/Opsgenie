bbc5a47df60d3968741b9c915bdf93b896c816aa
Mariano Obarrio Miles <mobarrio@aireuropa.com>
2022-12-13 12:10:03 +0100

    Ajustes en parametros de environment y nueva funcionalida de cerrado por message

--- deleteByName.py
+++ deleteByName.py
@@ -24,12 +24,14 @@ if(DEBUG):
   log.addHandler(stream)
   log.setLevel(logging.DEBUG)
 
+
+# Configure OPS connection
 configuration = opsgenie_sdk.Configuration()
-configuration.api_key['Authorization'] = os.environ.get('ops_api_key')
-configuration.host = os.environ.get('ops_api_url')
-api_instance = opsgenie_sdk.AlertApi(opsgenie_sdk.ApiClient(configuration))
+configuration.api_key['Authorization'] = os.environ.get('OPSGENIE_API_KEY')
+configuration.host = os.environ.get('OPSGENIE_API_URL')
+opsapi = opsgenie_sdk.AlertApi(opsgenie_sdk.ApiClient(configuration))
 identifier_type = 'id'
-close_alert_payload = opsgenie_sdk.CloseAlertPayload(source=os.environ.get('ops_closer_source'), user=os.environ.get('ops_closer_user'), note=os.environ.get('ops_closer_note'))
+close_alert_payload = opsgenie_sdk.CloseAlertPayload(source=os.environ.get('OPSGENIE_CLOSER_SOURCE'), user=os.environ.get('OPSGENIE_CLOSER_USER'), note=os.environ.get('OPSGENIE_CLOSER_NOTE'))
  
 zapi = ZabbixAPI(os.environ.get('zbx_api_url'))
 zapi.login(api_token=os.environ.get('zbx_api_token'))
@@ -55,11 +57,35 @@ def getProblemByName(host_name,problem_name):
             identifier = tag['value']
             save2log("getProblemByName - Hostname: [" + str(host_name) + "] - Problem: [" + str(problem_name) + "] - ID: [" + str(identifier) + "]")
             try:
-                api_response = api_instance.close_alert(identifier,identifier_type=identifier_type,close_alert_payload=close_alert_payload)
+                api_response = opsapi.close_alert(identifier,identifier_type=identifier_type,close_alert_payload=close_alert_payload)
             except Exception as e:
-                # print(e)
+                #print(e)
                 pass
+def ops_CountAlets(QueryString):
+   api_response = opsapi.count_alerts(query=QueryString)
+   return(api_response.data.count)
    
+def CloseProblemByMessage(msg):
+   try:
+      query = "status : open and message : *"+str(msg)+"*"
+      offset = 0
+      limit = 100
+      sort = 'createdAt'
+      order = 'desc'
+      countAlerts = ops_CountAlets(query)
+      max = int(round(countAlerts/100))+1
+      #print("Query : [",query,"] Records: [",countAlerts,"] Iteracciones: [",max,"]")
+      #print("Procesando",countAlerts,"registros.")
+      for i in range(0,max+1):
+         offset=(((max-i)*100))
+         api_response = opsapi.list_alerts(query=query, offset=offset, limit=limit, sort=sort, order=order)
+         for alert in api_response.data:
+            api_response = opsapi.close_alert(alert.id,identifier_type=identifier_type,close_alert_payload=close_alert_payload)
+            save2log("CloseProblemByHostname - Query: [" + str(msg) + "] - Problem: [" + str(alert.message) + "] - ID: [" + str(alert.id) + "]")
+   except Exception as e:
+      #print(e)
+      save2log("CloseProblemByHostname - Query: [" + str(msg) + "] - Problem: [" + str(alert.message) + "] - ID: [" + str(alert.id) + "] ERROR: ["+str(e)+"]")
+      pass
 
 def zbx_CloseActiveProblems(host_name):
    save2log("zbx_CloseActiveProblems - Hostname: [" + str(host_name) + "]")
@@ -78,11 +104,14 @@ def save2log(line):
 
 parser = argparse.ArgumentParser("settriggerdependency.py")
 parser.add_argument('-n', '--hostname', help="Nombre del Host en Zabbix", type=str)
+parser.add_argument('-m', '--message', help="Cierro problemas por nombre", type=str)
 args = parser.parse_args()
 
 if args.hostname:
    zbx_CloseActiveProblems(args.hostname)
    zbx_deleteHostByID(zbx_getHostID(args.hostname))
+elif args.message:
+   CloseProblemByMessage(args.message)
 else:
    print(parser.print_help())
 
